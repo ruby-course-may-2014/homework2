5e6567210b833deb29777bfa4becd1cc800bd732
Dmitriy Kiriyenko <dak@LG-TV.local>
2014-06-03 20:36:46 +0300

    Implement most of the tasks.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,10 @@
 class MoviesController < ApplicationController
+  helper_method :ratings_params, :all_ratings
 
   def index
-    @movies = Movie.all
+    session[:sort_by] = params[:sort_by] if params[:sort_by]
+    session[:ratings] = params[:ratings] if params[:ratings]
+    @movies = Movie.where(rating: ratings_params.keys).order(session[:sort_by])
   end
 
   def show
@@ -53,5 +56,12 @@ class MoviesController < ApplicationController
     params[:movie].permit(:title, :rating, :release_date, :description)
   end
 
-end
+  def all_ratings
+    @all_ratings ||= Movie.all_ratings
+  end
 
+  def ratings_params
+    session[:ratings] || Hash[all_ratings.map {|x| [x, "1"]}]
+  end
+
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,12 @@
 class Movie < ActiveRecord::Base
   validates :title, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true,
+                     inclusion: {in: ['G','PG','PG-13','R', 'NC-17'], allow_blank: true }
+  validates :release_date, presence: { message: "looks bad"}
+
+
+  def self.all_ratings
+    Movie.select(:rating).distinct.pluck(:rating)
+  end
+
 end
