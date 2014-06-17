140c732bf8b002eb17653a192c49f14f2505af0e
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2014-06-17 19:52:06 +0300

    Link user with movie.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140617163428) do
+ActiveRecord::Schema.define(version: 20140617164317) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20140617163428) do
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "movies", ["user_id"], name: "index_movies_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
